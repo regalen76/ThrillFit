d2c2607c4981d91a69e797288b993c251b358567
Adhi Arivianto <adhi.renaldy2@gmail.com>
2024-03-31 16:00:39 +0800

    Adjust profile page to have sign out function.

--- lib/screens/profile/profile_page.dart
+++ lib/screens/profile/profile_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:thrill_fit/services/auth.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -8,14 +10,43 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  final User? user = Auth().currentUser;
+
+  void signOut() async {
+    await Auth().signOut();
+  }
+
+  Widget userUID() {
+    return Text(user?.email ?? 'Not Signed In');
+  }
+
+  Widget signOutButton() {
+    return ElevatedButton(
+        onPressed: () {
+          signOut();
+          Navigator.pop(context);
+        },
+        child: const Text('Sign Out'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Profile'),
       ),
-      body: Center(
-        child: Text('Ini Profile Page'),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            userUID(),
+            signOutButton(),
+          ],
+        ),
       ),
     );
   }
