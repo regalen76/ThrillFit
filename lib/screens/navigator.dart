1887143987468186ac86a7d0d455a360068665d3
Adhi Arivianto <adhi.renaldy2@gmail.com>
2024-03-31 16:42:14 +0800

    Adjust Navbar splash effect.

--- lib/screens/navigator.dart
+++ lib/screens/navigator.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-import 'package:persistent_bottom_nav_bar_v2/persistent-tab-view.dart';
 import 'package:thrill_fit/screens/challenges/challenges_page.dart';
 import 'package:thrill_fit/screens/feeds/feeds_page.dart';
 import 'package:thrill_fit/screens/my_workout_plan/my_workout_plan_page.dart';
@@ -25,20 +24,31 @@ class _NavigatorPageState extends State<NavigatorPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: _screens[_currentTabIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        currentIndex: _currentTabIndex,
-        onTap: (int index) {
-          setState(() {
-            _currentTabIndex = index;
-          });
-        },
-        items: [
-          BottomNavigationBarItem(icon: Icon(MdiIcons.home), label: ''),
-          BottomNavigationBarItem(icon: Icon(MdiIcons.dumbbell), label: ''),
-          BottomNavigationBarItem(icon: Icon(MdiIcons.trophyAward), label: '')
-        ],
+      bottomNavigationBar: Theme(
+        data: ThemeData(
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+        ),
+        child: BottomNavigationBar(
+          showSelectedLabels: true,
+          showUnselectedLabels: false,
+          currentIndex: _currentTabIndex,
+          backgroundColor: Colors.black,
+          unselectedItemColor: Colors.grey,
+          selectedItemColor: Colors.blue,
+          onTap: (int index) {
+            setState(() {
+              _currentTabIndex = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(icon: Icon(MdiIcons.home), label: 'Feeds'),
+            BottomNavigationBarItem(
+                icon: Icon(MdiIcons.dumbbell), label: 'Workout Plans'),
+            BottomNavigationBarItem(
+                icon: Icon(MdiIcons.trophyAward), label: 'Challenges')
+          ],
+        ),
       ),
     );
   }
